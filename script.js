86eec7fb866844999f643c2d594f0d08afaf28b0
emmastanco <89658931+emmastanco@users.noreply.github.com>
2023-02-13 22:02:31 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var username = prompt("What is your name?");
+let placeholder = document.getElementById("username");
+placeholder.textContent= username;
+
+
+
+document.getElementById("changeGreena").onclick = function(){
+	document.getElementById("outputa").style.color = 'green';
+}
+
+document.getElementById("changeGreenb").onclick = function(){
+	document.getElementById("outputb").style.color = 'green';
+}
+
+document.getElementById("changeGreenc").onclick = function(){
+	document.getElementById("outputc").style.color = 'green';
+}
+
+document.getElementById("changeGreend").onclick = function(){
+	document.getElementById("outputd").style.color = 'green';
+}
+
+document.getElementById("changeGreene").onclick = function(){
+	document.getElementById("outpute").style.color = 'green';
+}
